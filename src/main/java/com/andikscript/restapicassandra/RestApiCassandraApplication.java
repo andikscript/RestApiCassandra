fcad2b3bebd419af83d77b84985d11ea144ebcb1
andikscript <andikscript@gmail.com>
2022-07-31 18:42:25 +0700

    initial commit

---
+++ src/main/java/com/andikscript/restapicassandra/RestApiCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.andikscript.restapicassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCassandraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andikscript/restapicassandra/RestApiCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andikscript.restapicassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
