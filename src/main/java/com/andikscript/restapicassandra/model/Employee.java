8fe622544f4f4d0f9488898f8bba5ad251f4ead8
andikscript <andikscript@gmail.com>
2022-07-31 18:47:46 +0700

    add model employee

---
+++ src/main/java/com/andikscript/restapicassandra/model/Employee.java
@@ -0,0 +1,40 @@
+package com.andikscript.restapicassandra.model;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@Table
+public class Employee {
+    @PrimaryKey
+    private UUID id;
+
+    private String name;
+
+    private Integer salary;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
